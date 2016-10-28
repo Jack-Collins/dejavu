65d5280f0d9fd53ee229a80687be67b0c5840084
Jack Collins <jackendacollins@gmail.com>
2016-10-28 21:53:12 +0100

    Update database_orchestrate.py

--- dejavu/database_orchestrate.py
+++ dejavu/database_orchestrate.py
@@ -4,11 +4,11 @@ from porc import Patch
 
 from dejavu.database import Database
 
-client = new Client("7e23e64c-f1a8-4072-90c7-6c00c804c0e5")
-
 class OrchestrateDatabase(Database):
     type = "orchestrate"
 
+    client = new Client("7e23e64c-f1a8-4072-90c7-6c00c804c0e5")
+    
      # tables
         FINGERPRINTS_TABLENAME = "fingerprints"
         SONGS_TABLENAME = "songs"
