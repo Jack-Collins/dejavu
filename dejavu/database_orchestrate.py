da6b7b4bca0bf87e5c9bc50fec5c0acac98b545d
Jack Collins <jackendacollins@gmail.com>
2016-10-28 22:52:36 +0100

    Update database_orchestrate.py

--- dejavu/database_orchestrate.py
+++ dejavu/database_orchestrate.py
@@ -6,17 +6,15 @@ from porc import Patch
 from dejavu.database import Database
 
 client = Client("7e23e64c-f1a8-4072-90c7-6c00c804c0e5")
+# tables
+FINGERPRINTS_TABLENAME = "fingerprints"
+SONGS_TABLENAME = "songs"
 
+# fields
+FIELD_FINGERPRINTED = "fingerprinted"
     
 class OrchestrateDatabase(Database):
     type = "orchestrate"
-    
-    # tables
-    FINGERPRINTS_TABLENAME = "fingerprints"
-    SONGS_TABLENAME = "songs"
-
-    # fields
-    FIELD_FINGERPRINTED = "fingerprinted"
 
     def __init__(self, **options):
             super(OrchestrateDatabase, self).__init__()
