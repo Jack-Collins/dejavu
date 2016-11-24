833fd4b339da0f5450f8bc7048fe44aaef320ce4
Jack Collins <jackendacollins@gmail.com>
2016-11-24 21:10:07 +0000

    Update example-overlay.py

--- example-overlay.py
+++ example-overlay.py
@@ -1,9 +1,10 @@
 import warnings
 import json
 warnings.filterwarnings("ignore")
-
+from dejavu.testing import *
 from dejavu import Dejavu
 from dejavu.recognize import FileRecognizer, MicrophoneRecognizer
+from pydub import AudioSegment
 
 # load config from a JSON file (or anything outputting a python dictionary)
 with open("dejavu.cnf.SAMPLE") as f:
@@ -11,6 +12,20 @@ with open("dejavu.cnf.SAMPLE") as f:
 
 if __name__ == '__main__':
 
+	#read in audio files
+	sound1 = AudioSegment.from_mp3("mp3/Over - Kings of Leon.mp3")
+	sound2 = AudioSegment.from_mp3("mp3/Relaxing Fan White Noise For Sleeping, Studying, Soothing Crying Baby, Insomnia.mp3")
+
+	sound1_10_db_quieter = sound1 - 10
+	sound2_very_loud = sound2 + 36
+
+	#overlay the loud white noise on the quiet song:
+	output = sound1_10_db_quieter.overlay(sound2_very_loud, position = 0)
+
+	#save resulting audio:
+
+	output.export("overlays/overlayed-track.mp3", format="mp3")
+	
 	# create a Dejavu instance
 	djv = Dejavu(config)
 
@@ -24,4 +39,5 @@ if __name__ == '__main__':
 	# Recognize audio from a file with white noise overlayed on top of it
 	song = djv.recognize(FileRecognizer, "overlays/overlayed-track.mp3")
 	print "From file we recognized: %s\n" % song
+	
 	
