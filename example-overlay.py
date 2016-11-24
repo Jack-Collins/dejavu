799739b6dd9bc565a38c8609960dc99254386bd0
Jack Collins <jackendacollins@gmail.com>
2016-11-24 20:17:34 +0000

    Create example-overlay.py

---
+++ example-overlay.py
@@ -0,0 +1,23 @@
+import warnings
+import json
+warnings.filterwarnings("ignore")
+
+from dejavu import Dejavu
+from dejavu.recognize import FileRecognizer, MicrophoneRecognizer
+
+# load config from a JSON file (or anything outputting a python dictionary)
+with open("dejavu.cnf.SAMPLE") as f:
+    config = json.load(f)
+
+if __name__ == '__main__':
+
+	# create a Dejavu instance
+	djv = Dejavu(config)
+
+	# Fingerprint all the mp3's in the directory we give it
+	djv.fingerprint_directory("mp3", [".mp3"])
+
+	# Recognize audio from a file with white noise overlayed on top of it
+	song = djv.recognize(FileRecognizer, "overlays/overlayed-track.mp3")
+	print "From file we recognized: %s\n" % song
+	
