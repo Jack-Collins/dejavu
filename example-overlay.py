fb84977c0653403adefaf8ceb226ff1ab0c21ba1
Jack Collins <jackendacollins@gmail.com>
2016-11-24 20:37:43 +0000

    Update example-overlay.py

--- example-overlay.py
+++ example-overlay.py
@@ -17,6 +17,10 @@ if __name__ == '__main__':
 	# Fingerprint all the mp3's in the directory we give it
 	djv.fingerprint_directory("mp3", [".mp3"])
 
+	# Recognize audio from its original file
+	song = djv.recognize(FileRecognizer, "mp3/Over - Kings of Leon.mp3")
+	print "From file we recognized: %s\n" % song
+	
 	# Recognize audio from a file with white noise overlayed on top of it
 	song = djv.recognize(FileRecognizer, "overlays/overlayed-track.mp3")
 	print "From file we recognized: %s\n" % song
