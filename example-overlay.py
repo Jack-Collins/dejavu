2a48d283b006928909b2a132fd3dc2dbef7debcc
Jack Collins <jackendacollins"gmail.com>
2016-12-23 11:29:09 +0000

    further automation of robust testing

--- example-overlay.py
+++ example-overlay.py
@@ -1,40 +1,68 @@
 import warnings
 import json
+import random
+import csv
 warnings.filterwarnings("ignore")
 from dejavu.testing import *
 from dejavu import Dejavu
 from dejavu.recognize import FileRecognizer, MicrophoneRecognizer
 from pydub import AudioSegment
 
+def composeTestCase(pathToOriginalSong, timeSnippet, originalSongVolumeShift, overlayedNoiseVolumeShifts):
+	print("File: %s" % pathToOriginalSong, "Attributes: %s" % str(originalSongVolumeShift) + "," + str(overlayedNoiseVolumeShifts.values()))
+	originalAudio = AudioSegment.from_mp3(pathToOriginalSong)
+	audioSnippet = trimAudio(originalAudio, timeSnippet, 10000)
+	originalSnippet = audioSnippet
+	audioSnippet = audioSnippet  + originalSongVolumeShift
+	for noise in overlayedNoiseVolumeShifts.keys():
+		overlay = AudioSegment.from_mp3(noise)
+		overlaySnippet = trimAudio(overlay, timeSnippet, 10000)
+		overlaySnippet = overlaySnippet + overlayedNoiseVolumeShifts[noise]
+    		audioSnippet = audioSnippet.overlay(overlaySnippet, position = 0)
+	return [originalSnippet, audioSnippet]
+	
+
+
+def trimAudio(audio, time, padding):
+	randomPosition = random.randint(padding, len(audio) - padding - time)
+	audio = audio[randomPosition:randomPosition + time]
+	return audio
+	
+	
+
 # load config from a JSON file (or anything outputting a python dictionary)
 with open("dejavu.cnf.SAMPLE") as f:
     config = json.load(f)
 
 if __name__ == '__main__':
-	#generate_test_files("mp3", "overlays", 5, fmts=[".mp3", ".wav"], padding=10)
-	#read in audio files
-	sound1 = AudioSegment.from_mp3("overlays/Over - Kings of Leon_88_5sec.mp3")
-	sound2 = AudioSegment.from_mp3("overlays/Relaxing Fan White Noise For Sleeping, Studying, Soothing Crying Baby, Insomnia_22_5sec.mp3")
+	with open("results.csv", "wb") as csvfile:
+		for i in range(10000):
+			randomTimeSnippet = random.randint(100, 10000)
+			randomOriginalSoundLevel = random.randint(-30, 20)
+			randomWhiteNoiseLevel = random.randint(-3000, -200)
 
-	sound1_10_db_quieter = sound1
-	sound2_very_loud = sound2 +20
-
-	#overlay the loud white noise on the quiet song:
-	output = sound2_very_loud.overlay(sound1_10_db_quieter, position = 0)
-
-	#save resulting audio:
-
-	output.export("overlays/overlayed-track.mp3", format="mp3")
+			#compose test case
+			snippets = composeTestCase("mp3/Sean-Fournier--Falling-For-You.mp3", randomTimeSnippet, randomOriginalSoundLevel, {"mp3/Relaxing Fan White Noise For Sleeping, Studying, Soothing Crying Baby, Insomnia.mp3" : randomWhiteNoiseLevel})
 	
-	# create a Dejavu instance
-	djv = Dejavu(config)
+			snippets[0].export("overlays/original-snippet.mp3", format="mp3")
+			snippets[1].export("overlays/overlayed-track.mp3", format="mp3")
+	
+			#save resulting audio:
+	
+			# create a Dejavu instance
+			djv = Dejavu(config)
 
-	# Recognize audio from its original file
-	song = djv.recognize(FileRecognizer, "overlays/Over - Kings of Leon_88_5sec.mp3")
-	print "From file we recognized: %s\n" % song
+			# Recognize audio from its original file
+			originalAudio = djv.recognize(FileRecognizer, "overlays/original-snippet.mp3")
+			#print "From file we recognized: %s\n" % originalAudio
 	
-	# Recognize audio from a file with white noise overlayed on top of it
-	song = djv.recognize(FileRecognizer, "overlays/overlayed-track.mp3")
-	print "From file we recognized: %s\n" % song
-	
-	
+			# Recognize audio from a file with various noises overlayed on top of it
+			overlayedAudio = djv.recognize(FileRecognizer, "overlays/overlayed-track.mp3")
+			#print "From file we recognized: %s\n" % overlayedAudio
+			if(originalAudio["song_id"] == overlayedAudio["song_id"]):
+				print "Percentage of fingerprints retained %s" % (overlayedAudio["confidence"]*100.0/originalAudio["confidence"] )
+				writer = csv.writer(csvfile, delimiter=',')
+				writer.writerow([randomTimeSnippet, randomOriginalSoundLevel, randomWhiteNoiseLevel, (overlayedAudio["confidence"]*100.0/originalAudio["confidence"] )])
+			else:
+				print "Incorrect match"
+	i
