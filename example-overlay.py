0d9a789c719a0a349938a1dd2f01ab69ada082a3
Jack Collins <jackendacollins"gmail.com>
2017-01-11 01:36:36 +0000

    restructuring directories

--- example-overlay.py
+++ example-overlay.py
@@ -13,6 +13,7 @@ def composeTestCase(pathToOriginalSong, timeSnippet, originalSongVolumeShift, ov
 	originalAudio = AudioSegment.from_mp3(pathToOriginalSong)
 	audioSnippet = trimAudio(originalAudio, timeSnippet, 10000)
 	originalSnippet = audioSnippet
+	
 	audioSnippet = audioSnippet  + originalSongVolumeShift
 	for noise in overlayedNoiseVolumeShifts.keys():
 		overlay = AudioSegment.from_mp3(noise)
@@ -39,15 +40,21 @@ if __name__ == '__main__':
 		for i in range(1000):
 			randomTimeSnippet = random.randint(1000, 10000)
 			randomOriginalSoundLevel = random.randint(-60, 50)
-			randomWhiteNoiseLevel = random.randint(-60, -50)
+			randomWhiteNoiseLevel = random.randint(-60, 50)
+			randomCoffeeShopNoiseLevel = random.randint(-60, 50)
 
 			#compose test case
-			snippets = composeTestCase("mp3/Sean-Fournier--Falling-For-You.mp3", randomTimeSnippet, randomOriginalSoundLevel, {"mp3/Relaxing Fan White Noise For Sleeping, Studying, Soothing Crying Baby, Insomnia.mp3" : randomWhiteNoiseLevel})
-	
+			snippets = composeTestCase("mp3/Sean-Fournier--Falling-For-You.mp3", randomTimeSnippet, randomOriginalSoundLevel, {"overlays/Relaxing Fan White Noise For Sleeping, Studying, Soothing Crying Baby, Insomnia.mp3" : randomWhiteNoiseLevel, "overlays/Coffee Shop Sound Effect.mp3" : randomCoffeeShopNoiseLevel})
+			
+			#save resulting audio:
 			snippets[0].export("overlays/original-snippet.mp3", format="mp3")
 			snippets[1].export("overlays/overlayed-track.mp3", format="mp3")
-	
-			#save resulting audio:
+			
+			#Note original dBFS value of snippet along with dBFS change applied:
+			originalSnippet = AudioSegment.from_mp3("overlays/original-snippet.mp3")
+			audioSnippet = AudioSegment.from_mp3("overlays/overlayed-track.mp3")
+			dBFSOriginal = 	originalSnippet.dBFS	
+			dBFSChange = audioSnippet.dBFS - dBFSOriginal
 	
 			# create a Dejavu instance
 			djv = Dejavu(config)
@@ -59,15 +66,15 @@ if __name__ == '__main__':
 			# Recognize audio from a file with various noises overlayed on top of it
 			overlayedAudio = djv.recognize(FileRecognizer, "overlays/overlayed-track.mp3")
 			#print "From file we recognized: %s\n" % overlayedAudio
+			writer = csv.writer(csvfile, delimiter=',')
 			try:
 				if(originalAudio["song_id"] == overlayedAudio["song_id"]):
 					print "Percentage of fingerprints retained %s" % (overlayedAudio["confidence"]*100.0/originalAudio["confidence"] )
-					writer = csv.writer(csvfile, delimiter=',')
-					writer.writerow([randomTimeSnippet, randomOriginalSoundLevel, randomWhiteNoiseLevel, (overlayedAudio["confidence"]*100.0/originalAudio["confidence"] ), 'CORRECT'])
+					writer.writerow([dBFSOriginal, dBFSChange, randomTimeSnippet, randomWhiteNoiseLevel, randomCoffeeShopNoiseLevel, (overlayedAudio["confidence"]*100.0/originalAudio["confidence"] ), 'CORRECT'])
 				else:
 					print "Incorrect match"
-					writer.writerow([randomTimeSnippet, randomOriginalSoundLevel, randomWhiteNoiseLevel, 0, 'INCORRECT'])
+					writer.writerow([dBFSOriginal, dBFSChange, randomTimeSnippet, randomWhiteNoiseLevel, randomCoffeeShopNoiseLevel, 0, 'INCORRECT'])
 			except TypeError:
 				print "Incorrect match"
-				writer.writerow([randomTimeSnippet, randomOriginalSoundLevel, randomWhiteNoiseLevel, 0, 'INCORRECT'])
+				writer.writerow([dBFSOriginal, dBFSChange, randomTimeSnippet, randomWhiteNoiseLevel, randomCoffeeShopNoiseLevel, 0, 'INCORRECT'])
 		i
